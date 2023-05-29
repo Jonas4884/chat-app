f866db5e29d3a5ba1f4aa2c20f1e5eb13d880aa8
Jonatana RANDRIAMPARANY Andrianina <Jonas4884>
2023-05-30 00:33:33 +0300

    chore(to-fix): menu dropdown under three themes

--- src/common/components/sidebar/DropDownBar.tsx
+++ src/common/components/sidebar/DropDownBar.tsx
@@ -1,5 +1,6 @@
 import { Channel } from "@/common/types";
-import Dropdown from "react-bootstrap/Dropdown";
+import { Dropdown } from "@nextui-org/react";
+
 
 type SideBarProps = {
   type: string;
@@ -9,28 +10,53 @@ type SideBarProps = {
 export const DropdownArea = ({ type, data,status }: SideBarProps) => {
     return (
       <>
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
+        {/* <Dropdown>
+          <Dropdown.Button flat id="dropdown-basic">
             {type}
-          </Dropdown.Toggle>
+          </Dropdown.Button>
 
 {
   data ?(
-    <Dropdown.Menu>
+    <Dropdown.Menu aria-label="Static Actions">
     {data.map((key, value) => {
       return (
-        <Dropdown.Item key={key.id} href={`/${status}/${key.id}`}>
-          {key.name}
+        <>
+             <Dropdown.Item key={key.id} >
+              <a  >
+              {key.name}
+              </a>
+         
         </Dropdown.Item>
+        </>
+       
       );
     })}
+     <Dropdown.Item key="new">New file</Dropdown.Item>
+        <Dropdown.Item key="2">Copy link</Dropdown.Item>
+        <Dropdown.Item key="3">Edit file</Dropdown.Item>
+        <Dropdown.Item key="4" withDivider color="error">  Delete file
+        </Dropdown.Item>
   </Dropdown.Menu>
   ) : <>
   No {status}
   </>
   
 }
-</Dropdown>
+</Dropdown> */}
+  {/*
+  FIXME : change this dropdow, as the template in the button
+  */}
+ <Dropdown>
+      <Dropdown.Button flat>{type}</Dropdown.Button>
+      <Dropdown.Menu aria-label="Static Actions">
+        <Dropdown.Item key="new">User 1</Dropdown.Item>
+        <Dropdown.Item key="copy">Copy link</Dropdown.Item>
+        <Dropdown.Item key="edit">Edit file</Dropdown.Item>
+        <Dropdown.Item key="delete" withDivider color="error">
+          Delete file
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
 </>
     );
   }
